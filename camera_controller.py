6f1bfc21ef4fb1b2b8c6178a6ff93212364140a4
sanchezcarlosjr <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-04-12 03:06:32 -0700

    feat: you can use opencv with a decent syntax

--- camera_controller.py
+++ camera_controller.py
@@ -3,35 +3,31 @@ import json
 import asyncio_mqtt as aiomqtt
 import base64
 
-# https://gist.github.com/khalidmeister/ea292150905d748457d8f6c19ec095c3
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(4)
+cap = cv2.VideoCapture(0)
+resize_width = 320
+resize_height = 240
+fps = 20 # frames per second
+frame_interval = 1/fps # second per frame
+MQTT_QOS=0
 
-async def publish_camera_device(client):
+async def publish_video_from_camera(client):
   while cap.isOpened():
-      success, image = cap.read()
-
-      # Flip the image horizontally for a later selfie-view display
-      # Also convert the color space from BGR to RGB
-      image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-
-      # Convert the color space from RGB to BGR
-      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-      img_h, img_w, img_c = image.shape
-
-      retval, buffer = cv2.imencode('.jpg', image)
-      base64_image = base64.b64encode(buffer).decode('utf-8')
-
-      await client.publish("eva/camera/image", payload=base64_image)
-      await asyncio.sleep(0.01)
+      success, frame = cap.read()
+      # TODO: Should we change to gray scale?
+      frame = cv2.cvtColor(cv2.flip(frame, 1), cv2.COLOR_BGR2RGBA)
+      frame = cv2.resize(frame, (resize_width, resize_height))
+      retval, buffer = cv2.imencode('.jpg', frame)
+      if retval:
+          # TODO: Should we compress with Brotli?
+         await client.publish("eva/camera/image", payload=base64.b64encode(buffer), qos=MQTT_QOS)
+      await asyncio.sleep(frame_interval)
 
 async def listen():
-    async with aiomqtt.Client("158.97.91.177", 8883, username = "eva_cicese", password = "NQz4esJX$") as client:
-         await publish_camera_device(client)
+    async with aiomqtt.Client("localhost", 8883) as client:
+         await publish_video_from_camera(client)
 
 
 asyncio.run(listen())
-
